2eb7e87c6ed3d38df5884d89362dc8b28442b18b
priya718 <69306019+priya718@users.noreply.github.com>
2020-10-19 17:07:59 +0530

    Create pull_request_template.m

---
+++ pull_request_template.m
@@ -0,0 +1,11 @@
+# hello everyone
+## Description
+ we are here to learn git 
+ concentrate and learn easily
+ 
+### Result
+  the reult will be fantastic if you learn good.
+  have a happy learning...
+  
+[] bug fix
+  
